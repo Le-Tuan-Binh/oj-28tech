29577313f80496cdad028afa0df16c7e9d351d10
Lê Tuấn Bình <TBinTB0003@gmail.com>
2024-06-21 14:09:46 +0700

    Update the syntax

--- oj-28tech/java/Cấu trúc rẽ nhánh & Toán tử/ifelse07.java
+++ oj-28tech/java/Cấu trúc rẽ nhánh & Toán tử/ifelse07.java
@@ -5,15 +5,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         
         // Reading input
-        long a = sc.nextLong();
-        long b = sc.nextLong();
+        long a = sc.nextLong(),  b = sc.nextLong();
         
-        // Performing calculations
-        long result_one = a / b * b;
-        long result_two = (a + b - 1) / b * b;
+        // Find the largest number <= a that is divisible by b
+        long largest_divisible = a / b * b;
+
+        // Find the smallest number >= a that is divisible by b
+        long smallest_divisible = (a + b - 1) / b * b;
         
         // Outputting results
-        System.out.print(result_one + " " + result_two);
+        System.out.print(largest_divisible + " " + smallest_divisible);
 
         // Closing scanner (not essential in this case, but good practice)
         sc.close();
