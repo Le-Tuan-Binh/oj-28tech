b5690cf3a5059d74d87ea42993054bc29fb9ae58
Lê Tuấn Bình <TBinTB0003@gmail.com>
2024-06-21 13:58:01 +0700

    Create first file problem in OJ

---
+++ oj-28tech/Làm quen với Online Judge/oj01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int x;
+    std::cin >> x;
+    std::cout << x << std::endl;
+    std::cout << "Hello World !" << std::endl;
+    std::cout << "28tech C++ programming !" << std::endl;
+    return 0;
+}
